55d3a9bd95eb6a7df7ce746523ce9a4989c86a08
maninder-mike <maninder.singh@outlook.in>
2020-05-12 16:27:52 +0300

    gitignore modification

--- scrap.py
+++
@@ -1,5 +0,0 @@
-from pip_lock import check_requirements, get_mismatches
-
-m = get_mismatches('requirements.txt')
-
-print(check_requirements('requirements.txt',post_text='\nRun the following on your host machine: \n\nvagrant provision\n'))
